b2129d0bedb3e5a3018f3d289b782c66e356f0d2
Mario Viapiano <mario.viapiano@globallogic.com>
2017-02-14 13:33:40 -0300

    more info in message

--- app.rb
+++ app.rb
@@ -2,9 +2,14 @@ require 'sinatra'
 require 'httparty'
 require 'nokogiri'
 require './wtf/scraper'
+require 'erb'
 
 post '/' do
-  term = params.fetch('text').strip
+  @term = params.fetch('text').strip
+  @definition = WTF::Scraper.(@term)
 
-  WTF::Scraper.(term)
+  # template = File.open('./templates/basic.erb')
+  template = "<%= @term.upcase %>:\n <%= @definition %>"
+
+  ERB.new(template).result(binding)
 end
