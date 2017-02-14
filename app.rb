3769792bba29e4b618669cb733681f7032e26a62
Mario Viapiano <mario.viapiano@globallogic.com>
2017-02-14 14:44:18 -0300

    Basic refactor

--- app.rb
+++ app.rb
@@ -4,12 +4,14 @@ require 'nokogiri'
 require './wtf/scraper'
 require 'erb'
 
+APP_ROOT = File.dirname(__FILE__)
+BASIC_TEMPLATE_PATH = File.join(APP_ROOT, 'templates/basic.erb')
+
 post '/' do
   @term = params.fetch('text').strip
   @definition = WTF::Scraper.(@term)
 
-  # template = File.open('./templates/basic.erb')
-  template = "<%= @term.upcase %>:\n <%= @definition %>"
+  template = File.read(BASIC_TEMPLATE_PATH)
 
   ERB.new(template).result(binding)
 end
